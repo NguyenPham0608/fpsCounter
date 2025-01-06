ac6367a0bbe37db1a7569e52bf343a81e80b471b
Nguyen Pham <nguyenpham0806@gmail.com>
2025-01-06 11:06:35 -0800

    Added crash

--- fps.js
+++ fps.js
@@ -3,6 +3,7 @@ let fps = 0;
 let averageFps = 0;
 const fpsDisplay = document.getElementById('fps');
 const avgFpsDisplay = document.getElementById('avgFps');
+let kill=2
 
 const fpsArray = [];
 
@@ -27,6 +28,25 @@ function calculateFPS(timestamp) {
     avgFpsDisplay.textContent = `AVERAGE FPS: ${averageFps}`;
 
     requestAnimationFrame(calculateFPS); // Keep the loop going
+}
+
+window.addEventListener('keydown', function(e){
+    if (e.key == ' ') {
+        degradePerformance()
+
+    }
+})
+let interval;
+function degradePerformance() {
+    const container = document.createElement("div");
+    document.body.appendChild(container);
+    interval = setInterval(() => {
+        for (let i = 0; i < 300; i++) {
+            const div = document.createElement("div");
+            container.appendChild(div);
+        }
+        console.log("Added 300 elements.");
+    }, 10); // Adds 300 divs every 10ms
 }
 
 requestAnimationFrame(calculateFPS); // Start the loop
